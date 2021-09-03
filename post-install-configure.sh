5577c8bb2930232593e070b223dfc3216643d852
Brad Klein <bklein@cray.com>
2021-09-03 10:53:24 -0600

    Change service name to have dashes instead of underbars, update README

--- post-install-configure.sh
+++ post-install-configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-sed -e "s/10.252.0.7/$(hostname -i)/" >/etc/systemd/system/cray_node_exporter.service <<'EOF'
+sed -e "s/10.252.0.7/$(hostname -i)/" >/etc/systemd/system/cray-node-exporter.service <<'EOF'
 
 [Unit]
 Description=Node Exporter
@@ -21,10 +21,10 @@ ExecStart=/usr/bin/node_exporter \
 --collector.qdisc \
 --collector.systemd \
 --collector.tcpstat \
---collector.textfile.directory /var/lib/cray_node_exporter/textfile_collector
+--collector.textfile.directory /var/lib/cray-node-exporter/textfile_collector
 #--collector.filesystem.ignored-mount-points '^/(dev|proc|sys|var/lib/docker/.+)($|/)' \
 #--collector.filesystem.ignored-fs-types '^(autofs|binfmt_misc|cgroup|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|mqueue|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|sysfs|tracefs)$' \
-SyslogIdentifier=cray_node_exporter
+SyslogIdentifier=cray-node-exporter
 Restart=always
 RestartSec=1
 
@@ -32,10 +32,10 @@ RestartSec=1
 WantedBy=multi-user.target
 EOF
 
-chown root:root /etc/systemd/system/cray_node_exporter.service
+chown root:root /etc/systemd/system/cray-node-exporter.service
 
-chmod 644 /etc/systemd/system/cray_node_exporter.service
+chmod 644 /etc/systemd/system/cray-node-exporter.service
 
-systemctl start cray_node_exporter
+systemctl start cray-node-exporter
 
-systemctl status cray_node_exporter
+systemctl status cray-node-exporter
